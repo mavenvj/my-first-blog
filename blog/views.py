f9eaaa87bf1c4314c054bab12c8bc270238ca2bd
mavenvj <vidajue@gmail.com>
2021-08-16 14:53:29 -0500

    changed views & post_list html

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from .models import Post
 
 # Create your views here.
-def post_list(request, pk):
-    posts = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/postz-detail.html', {'posts': posts})
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
