d29fad664acc32b9ab637398f43e921715f531b9
mavenvj <vidajue@gmail.com>
2021-08-17 16:24:10 -0500

    changed posts:posts to posts:post

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,9 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 # Create your views here.
-def post_list(request, pk):
+def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': post})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
